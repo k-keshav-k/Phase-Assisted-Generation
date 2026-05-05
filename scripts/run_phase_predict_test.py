ec9c38570b7736e27ff44ec9fd3b3a2251f702ab
Siva Satvik Mandapati <sivasatvik@gmail.com>
2026-05-05 11:07:53 -0400

    Add a checkpoint with 3 feature training and test script on how to use it

--- scripts/run_phase_predict_test.py
+++ scripts/run_phase_predict_test.py
@@ -13,7 +13,7 @@ from phase_predict.predict import Predictor
 from phase_predict.schema import PhaseTuple, ExtendedPhaseTuple
 
 
-CHECKPOINT_PATH = "output/ablations/medium_ws67_d64_h2_l1_dp0_lr1.0_bestval=0.488357.pt"
+CHECKPOINT_PATH = "output/ablations/large_ws67_d256_h8_l3_dp10_lr0.5_bestval=0.412367.pt"
 # CHECKPOINT_PATH = "output/ablations/medium_ws67_d64_h2_l1_dp0_lr1.0_bestval=0.109380.pt"
 
 print("1. Initializing synthetic test data...")
@@ -74,6 +74,12 @@ tuple2 = [ExtendedPhaseTuple(values={
 })]
 tuple3 = [ExtendedPhaseTuple(values={'block_size': 18, 'nfe': 5, 'max_stab_step': 3}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 15, 'nfe': 7, 'max_stab_step': 2}), ExtendedPhaseTuple(values={'block_size': 11, 'nfe': 8, 'max_stab_step': 4}), ExtendedPhaseTuple(values={'block_size': 11, 'nfe': 5, 'max_stab_step': 2}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 7, 'max_stab_step': 3}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 17, 'nfe': 3, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 6, 'max_stab_step': 2}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 9, 'max_stab_step': 8}), ExtendedPhaseTuple(values={'block_size': 6, 'nfe': 3, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 8, 'max_stab_step': 5}), ExtendedPhaseTuple(values={'block_size': 29, 'nfe': 5, 'max_stab_step': 1}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 9, 'max_stab_step': 7}), ExtendedPhaseTuple(values={'block_size': 9, 'nfe': 4, 'max_stab_step': 2}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 29, 'nfe': 10, 'max_stab_step': 9}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 14, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 10, 'max_stab_step': 7}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 7, 'max_stab_step': 5}), ExtendedPhaseTuple(values={'block_size': 12, 'nfe': 3, 'max_stab_step': 1}), ExtendedPhaseTuple(values={'block_size': 28, 'nfe': 2, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 7, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 31, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 29, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 19, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 2, 'nfe': 2, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 29, 'nfe': 2, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 41, 'nfe': 2, 'max_stab_step': 1}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 1, 'nfe': 1, 'max_stab_step': 0}), ExtendedPhaseTuple(values={'block_size': 16, 'nfe': 3, 'max_stab_step': 1}), ExtendedPhaseTuple(values={'block_size': 3, 'nfe': 1, 'max_stab_step': 0})]
 
+tuple4 = [ExtendedPhaseTuple(values={
+    'block_size': 15,
+    'nfe': 14,
+    'max_stab_step': 10
+})]
+
 print("\n2. Loading the checkpoint model from output/...")
 predictor = Predictor.from_checkpoint(CHECKPOINT_PATH)
 
@@ -94,6 +100,12 @@ print(f"Predicted next tuple: {result.predicted_tuple}\n")
 
 context_sequence = tuple3[:]
 print(*tuple3, sep="\n")
+# Predict the next tuple
+result = predictor.predict(context_sequence)
+print(f"Predicted next tuple: {result.predicted_tuple}\n")
+
+context_sequence = tuple4[:]
+print(*tuple4, sep="\n")
 # Predict the next tuple
 result = predictor.predict(context_sequence)
 print(f"Predicted next tuple: {result.predicted_tuple}\n")
